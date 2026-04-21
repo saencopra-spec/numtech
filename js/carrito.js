b06e251bc5827c76b5dafd9cf83f5a02e1794267
liangso420-cell <liangso420@gmail.com>
2026-04-20 18:43:42 -0600

    Agrege todo lo que es la pagina de los productos

--- js/carrito.js
+++ js/carrito.js
@@ -1,1 +1,18 @@
- 
+ let current = 0;
+const track = document.getElementById('carouselTrack');
+const dots = document.querySelectorAll('.dot');
+const total = 3;
+
+function goTo(index) {
+  current = index;
+  track.style.transform = 'translateX(-' + (current * 100) + '%)';
+  dots.forEach((d, i) => d.classList.toggle('active', i === current));
+}
+
+function moveNext() {
+  goTo((current + 1) % total);
+}
+
+function movePrev() {
+  goTo((current - 1 + total) % total);
+}
